c6ecfd2006cd04f6a005d9b9a0e4482dcec97b3b
mStylias <57811193+mStylias@users.noreply.github.com>
2022-05-04 15:32:05 +0300

    Some progress
    Stuck due to .NET 6 changes making the video tutorial obsolete

--- GoodBooks/GoodBooks.Services/BookService.cs
+++ GoodBooks/GoodBooks.Services/BookService.cs
@@ -4,29 +4,37 @@ using GoodBooks.Data.Models;
 namespace GoodBooks.Services;
 public class BookService : IBookService
 {
-    private readonly GoodBooksDbContext _dbContext;
+    private readonly GoodBooksDbContext _db;
     public BookService(GoodBooksDbContext dbContext)
     {
-        _dbContext = dbContext;
+        _db = dbContext;
     }
 
     public void AddBook(Book book)
     {
-        throw new NotImplementedException();
+        _db.Add(book);
+        _db.SaveChanges();
     }
 
     public void DeleteBook(int bookID)
     {
-        throw new NotImplementedException();
+        var bookToDelete = _db.Books.Find(bookID);
+        if (bookToDelete != null)
+        {
+            _db.Remove(bookToDelete);
+            _db.SaveChanges();
+        }
+
+        throw new InvalidOperationException("Cant delete a book that doesn't exist");
     }
 
     public List<Book> GetAllBooks()
     {
-        throw new NotImplementedException();
+        return _db.Books.ToList();
     }
 
     public Book GetBook(int bookID)
     {
-        throw new NotImplementedException();
+        return _db.Books.Find(bookID);
     }
 }
--- GoodBooks/GoodBooks.Web/Program.cs
+++ GoodBooks/GoodBooks.Web/Program.cs
@@ -1,8 +1,17 @@
+using GoodBooks.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<GoodBooksDbContext>(options =>
+{
+    options.EnableDetailedErrors();
+    //options.UseNpgsql(Configuration.)
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
